3fe7f34cc155974d91d8e00c8cf354cd9aa464f5
Minh Bang <bradonleminhbang1@gmail.com>
2022-09-08 13:41:17 +0700

    Heroku fix global authenication JWT

--- bases/views.py
+++ bases/views.py
@@ -1,9 +1,6 @@
 # Django
 from django_filters.rest_framework import DjangoFilterBackend
 
-# JWT
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
 # Rest Framework
 import base64
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -11,7 +8,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
 class BaseViewSet(ModelViewSet):
-    authentication_classes = [JWTAuthentication]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     
     def list(self, request, *args, **kwargs):
