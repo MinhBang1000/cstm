1cd6c44061ffcc4f4665f953aef44918459f851b
Minh Bang <bradonleminhbang1@gmail.com>
2022-09-11 17:23:02 +0700

    Company app is done!

--- companies/owner/views.py
+++ companies/owner/views.py
@@ -1,20 +1,29 @@
+# Rest framework
+from rest_framework.serializers import ValidationError
+
 # Customize
 from bases.views import BaseViewSet
-from bases import permissions as base_permissions
+from bases import errors, permissions as base_permissions
 from companies.owner import serializers as owner_serializer
 from companies.models import Company
 
 class CompanyViewSet(BaseViewSet):
 
     serializer_class = owner_serializer.CompanySerializer
-    permission_classes = [ base_permissions.IsOwnerAnonymus ]
+    permission_classes = [ base_permissions.IsOwnerAdmin ]
+    filterset_fields = [ "id", "company_name", "company_street", "company_district__district_name", "company_district__district_province__province_name" ]
     
     def get_queryset(self):
         return Company.objects.filter( company_owner = self.request.user )
     
     def perform_create(self, serializer):
         user = self.request.user
-        if user.role != "Owner":
+        if user.role == "Anonymus":
             user.role = "Owner"
             user.save()
         serializer.save( company_owner = user )
+
+    def update(self, request, *args, **kwargs):
+        if request.data.get("company_owner", False) != False:
+            raise ValidationError(errors.get_error(errors.COMPANY_CHANGE_OWNER))
+        return super().update(request, *args, **kwargs)
