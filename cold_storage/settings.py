dd1dcb64991b32e7a5fed658ec3d96481a82f0f2
Minh Bang <bradonleminhbang1@gmail.com>
2022-10-28 14:03:53 +0700

    Changing database for faster

--- cold_storage/settings.py
+++ cold_storage/settings.py
@@ -94,15 +94,27 @@ WSGI_APPLICATION = 'cold_storage.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'djongo',
+#             'NAME': os.getenv('DB_NAME'),
+#             'ENFORCE_SCHEMA': False,
+#             'CLIENT': {
+#                 'host': os.getenv('DB_URL')
+#             }  
+#         }
+# }
+
+# postgres sql
 DATABASES = {
-        'default': {
-            'ENGINE': 'djongo',
-            'NAME': os.getenv('DB_NAME'),
-            'ENFORCE_SCHEMA': False,
-            'CLIENT': {
-                'host': os.getenv('DB_URL')
-            }  
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 
