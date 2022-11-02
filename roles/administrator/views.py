ba459a5741c7b65183129f1987e75358a61cd0d1
Minh Bang <bradonleminhbang1@gmail.com>
2022-11-02 10:35:47 +0700

    Testing time

--- roles/administrator/views.py
+++ roles/administrator/views.py
@@ -25,26 +25,26 @@ class RoleViewSet(BaseViewSet):
             raise ValidationError(errors.get_error(errors.DO_NOT_PERMISSION))   
         return super().destroy(request, *args, **kwargs)
 
-    def perform_create(self, serializer):
-        role_creater = 0
-        if self.request.data.get("role_creater", None) != None:
-            role_creater = self.request.data.get("role_creater", None)
-        if self.request.user != None:
-            if self.request.user.role.id != 1 and self.request.user.role.role_creater == -1: # Là owner mới có quyền tạo role 
-                owner_permissions = self.request.user.role.role_permissions.all()
-                owner_permission_ids = [ per.id for per in owner_permissions ] 
-                wish_permissions = self.request.data.get('role_permissions', None)
-                if wish_permissions != None:
-                    for id in wish_permissions:
-                        if id not in owner_permission_ids:
-                            raise ValidationError(errors.get_error(errors.DO_NOT_HAVE_PERMISSION))
-                serializer.save(role_creater = self.request.user.id)
-            elif self.request.user.role.id == 1: # Admin
-                serializer.save(role_creater = role_creater)
-            else:
-                raise ValidationError(errors.get_error(errors.ONLY_OWNER_ADMIN))
-        else:
-            raise ValidationError(errors.get_error(errors.ONLY_OWNER_ADMIN))
+    # def perform_create(self, serializer):
+    #     role_creater = 0
+    #     if self.request.data.get("role_creater", None) != None:
+    #         role_creater = self.request.data.get("role_creater", None)
+    #     if self.request.user != None:
+    #         if self.request.user.role.id != 1 and self.request.user.role.role_creater == -1: # Là owner mới có quyền tạo role 
+    #             owner_permissions = self.request.user.role.role_permissions.all()
+    #             owner_permission_ids = [ per.id for per in owner_permissions ] 
+    #             wish_permissions = self.request.data.get('role_permissions', None)
+    #             if wish_permissions != None:
+    #                 for id in wish_permissions:
+    #                     if id not in owner_permission_ids:
+    #                         raise ValidationError(errors.get_error(errors.DO_NOT_HAVE_PERMISSION))
+    #             serializer.save(role_creater = self.request.user.id)
+    #         elif self.request.user.role.id == 1: # Admin
+    #             serializer.save(role_creater = role_creater)
+    #         else:
+    #             raise ValidationError(errors.get_error(errors.ONLY_OWNER_ADMIN))
+    #     else:
+    #         raise ValidationError(errors.get_error(errors.ONLY_OWNER_ADMIN))
         
     def perform_update(self, serializer):
         if self.request.user != None:
